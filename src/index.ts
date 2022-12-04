5ab0f20e35aeb12765ca0736057ff8646ab1621a
Ronit Panda <pandaronit25@gmail.com>
2022-12-04 10:14:41 +0530

    feat: added errors and middlewares to deal with auth and various general cases

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-interface Color {
-    red: number;
-    green: number;
-    blue: number;
-}
+/*=============================================
+=            Errors            =
+=============================================*/
 
-const color: Color = {
-    red: 255,
-    green: 0,
-    blue: 10,
-};
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-console.log(color);
+/*=====  End of Errors  ======*/
 
-export default color;
+/*=============================================
+=            Middlewares            =
+=============================================*/
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
+
+/*=====  End of Middlewares  ======*/
