beeffd487a463f5763a2515621e3602afdb4fdc9
Ronit Panda <pandaronit25@gmail.com>
2022-12-06 08:50:07 +0530

    feat: hooked up nats streaming and events

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,22 @@ export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
 /*=====  End of Middlewares  ======*/
+
+/*=============================================
+=            Streaming            =
+=============================================*/
+
+export * from './streaming/base-listener';
+export * from './streaming/base-publisher';
+export * from './streaming/subjects';
+
+/*=====  End of Streaming  ======*/
+
+/*=============================================
+=            Events            =
+=============================================*/
+
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-evemt';
+
+/*=====  End of Events  ======*/
