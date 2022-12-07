3c55b16f8af8df4ddc3354fe9fece368e77e99b1
Ronit Panda <pandaronit25@gmail.com>
2022-12-07 11:25:08 +0530

    fix: typo in order-status enum

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -3,7 +3,7 @@ export enum OrderStatus {
     Created = 'created',
     // The ticket the order is trying to reserve has already been reserved, or when the user has cancelled the order
     // The order expires before payment
-    cancelled = 'cancelled',
+    Cancelled = 'cancelled',
     // The order has successfully reserved the ticket
     AwaitingPayment = 'awaiting:payment',
     // The order has reserved the ticket and the user has provided payment successfully
